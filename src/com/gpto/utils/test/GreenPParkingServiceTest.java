aca77c2bee4dbf3647d9ca4b4113dfc964beb009
Rodrigo Fraga <rodrigo.fraga@gmail.com>
2014-07-19 12:28:49 -0400

    adding GreenPParkingservicetest class

---
+++ src/com/gpto/utils/test/GreenPParkingServiceTest.java
@@ -0,0 +1,17 @@
+package com.gpto.utils.test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class GreenPParkingServiceTest extends TestCase{
+    
+    @Override
+    protected void runTest() throws Throwable {
+        // TODO Auto-generated method stub
+        super.runTest();
+    }
+   public void testService(){
+       Assert.assertNotNull("tjo");
+   }
+
+}
